da8854b19eab964c8d16c8ce8aec28bbfee224c9
FrancisA <francis.adepoju@hotmail.com>
2018-09-02 16:17:36 +0100

    modified fine name and added DOCSTRING statement now

--- first_file.py
+++ first_file.py
@@ -1,2 +1,8 @@
+'''
+Initial work
+Input file: none
+Output: bunch of print lines
+'''
 print('Hello World. This is usually the first one')
+print('file-name has been modified to conform to snake_case convention now')
 print('bye from Jupyter')
