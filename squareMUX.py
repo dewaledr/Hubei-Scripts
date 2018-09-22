1ea4e52983ca375e30f0d9f931fbd6f350361ab3
FrancisA <francis.adepoju@hotmail.com>
2018-09-22 23:12:50 +0100

    Last one saved now

--- squareMUX.py
+++ squareMUX.py
@@ -53,9 +53,10 @@ def genMUX(num):
 
 
 if __name__ == '__main__':
-    os.system("clear")
-    # Terminate the program if input arg is not an integer
+    os.system("clear")  # Clear screen..."cls" works too
+    
     print(f"Number of arguments = {len(sys.argv)}")
+    # Terminate the program if input arg is not an integer or more than 1
     while True:
         try:
             if len(sys.argv) == 2:    # If there was argv[1]. argv[0] is name of pgm
@@ -64,14 +65,10 @@ if __name__ == '__main__':
                 print("Incorrect number of argument supplied. Please try again...")
                 break
         except:
-            #if len(sys.argv) == 2:    # If there was argv[1]. argv[0] is name of pgm
             print(f"Input argument was: {sys.argv[1]}")
             print("..............")
             print("\tPlease try again. Ensure that the input argument is an integer...")
             print("..............")
-            #else:
-            #    print("No input argument supplied. Please try again...")
-            #    break
         else:
             # Success... Correct int arg supplied during pgm instantiation
             #if type(sys.argv[1]) == int:
